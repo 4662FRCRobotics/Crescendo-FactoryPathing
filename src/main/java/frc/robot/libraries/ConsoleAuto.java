772d0d9d950a60f5f38f3a408574c98804f67907
Tim Ohling <trohling@peak.org>
2024-07-06 19:53:49 -0700

    starting - copy from simple shooter

---
+++ src/main/java/frc/robot/libraries/ConsoleAuto.java
@@ -0,0 +1,33 @@
+package frc.robot.libraries;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj.GenericHID;
+
+/** Add your docs here. */
+public class ConsoleAuto extends GenericHID {
+
+    private static final int kPOV_SW_0 = 0;
+    private static final int kPOV_SW_1 = 1;
+
+    public ConsoleAuto(final int port) {
+        super(port);
+    }
+
+    public int getROT_SW_0() {
+        return this.getPOV(kPOV_SW_0) / 45;
+    }
+
+    public int getROT_SW_1() {
+        return this.getPOV(kPOV_SW_1) / 45;
+    }
+
+    public boolean getButton(int button) {
+        return this.getRawButton(button);
+    }
+
+    public BooleanSupplier getSwitchSupplier(int button) {
+        return () -> this.getRawButton(button);
+    }
+
+}
